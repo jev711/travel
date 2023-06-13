5bd23099efa3ffed1168e96cff6856591f07936d
unknown <jishnuvnair711@gmail.com>
2023-06-13 06:15:49 +0530

    bla

---
+++ travelproject/travelapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from travelapp import views
+
+urlpatterns = [
+   path('',views.demo,name='demo'),
+
+]
---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place, Person
+
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1=Person.objects.all()
+    return render(request,"index.html",{'result':obj,'result1':obj1})
+
+
